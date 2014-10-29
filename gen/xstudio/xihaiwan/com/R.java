7750f83820ace56ca6da20ccc47efa477023a8b3
yl <yl@126.com>
2014-10-29 23:50:13 +0800

    logictestok

--- gen/xstudio/xihaiwan/com/R.java
+++ gen/xstudio/xihaiwan/com/R.java
@@ -14,7 +14,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int content=0x7f050000;
+        public static final int breakfast=0x7f050007;
+        public static final int button1=0x7f050002;
+        public static final int button2=0x7f050003;
+        public static final int button3=0x7f050004;
+        public static final int button4=0x7f050005;
+        public static final int button5=0x7f050006;
+        public static final int content=0x7f050001;
+        public static final int lunch=0x7f050009;
+        public static final int morningtaste=0x7f050008;
+        public static final int noontaste=0x7f05000a;
+        public static final int spinner1=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/xstudio/xihaiwan/com/XihaiwanActivity.java
+++ src/xstudio/xihaiwan/com/XihaiwanActivity.java
@@ -1,11 +1,16 @@
 package xstudio.xihaiwan.com;
 
 import java.io.BufferedReader;
+
+import android.view.View;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -21,7 +26,13 @@ import org.jsoup.select.Elements;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class XihaiwanActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -30,85 +41,159 @@ public class XihaiwanActivity extends Activity {
 	private TextView content = null;
 	private String msgs = null;
 	private int linecount = 0;
+	private int linknum = 0;
+	private String[] linkstr = null;
+	private String[] week = null;
+	private String[] url = null;
+	private Spinner spinner = null;
+	private List<CharSequence> adapteweek = null;
+	private TextView breakfast = null;
+	private TextView morningtaste = null;
+	private TextView lunch = null;
+	private TextView noontaste = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
 		final Runnable updateThread = new Runnable() {
 
 			public void run() {
 				// 更新UI
 				if (msgs != null) {
-					content.setText(msgs);
+					adapteweek = new ArrayList<CharSequence>();
+					for (int i =0; i<linknum; i++)
+					{
+						adapteweek.add(week[i]);
+					}
+
+					// 建立Adapter并且绑定数据源
+					ArrayAdapter<CharSequence> _Adapter = new ArrayAdapter<CharSequence>(
+							XihaiwanActivity.this,
+							android.R.layout.simple_spinner_item, adapteweek);
+					_Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+					// 绑定 Adapter到控件
+					spinner.setAdapter(_Adapter);
+					breakfast.setText(msgs);
+					content.setText(linkstr[0]);
 				} else {
 					content.setText("数据错误！");
 				}
+
+
+
 			}
 
 		};
+
+
 
 		new Thread() {
 			public void run() {
 				//
 				content = (TextView) findViewById(R.id.content);
+				spinner = (Spinner) findViewById(R.id.spinner1);
+				breakfast = (TextView)findViewById(R.id.breakfast);
+				morningtaste = (TextView)findViewById(R.id.morningtaste);
+				lunch = (TextView)findViewById(R.id.lunch);
+				noontaste = (TextView)findViewById(R.id.noontaste);
 				String myString = null;
-				StringBuffer sff = new StringBuffer();//一定要new一个，我刚开始搞忘了，出不来。
-				try  
-				{  
-				         Document doc = Jsoup.connect("http://www.cnblogs.com/agileyanly/p/4052627.html").get();  
-				        Elements links = doc.select("a[href]");  
-				        //注意这里是Elements不是Element。同理getElementById返回Element，getElementsByClass返回时Elements  
-				         for(Element link : links){  
-				              //这里没有什么好说的。  
-				             sff.append(link.attr("abs:href")).append("  ").append(link.text()).append(" ");  
-				            }  
-				          myString = sff.toString();  
-				     }  
-				       catch (Exception e)  
-				      {  
-				    	   myString = e.getMessage();  
-				           e.printStackTrace();  
-				       }  
-
-				
-				
-				
-				HttpGet httpget = new HttpGet(
-						"http://www.cnblogs.com/agileyanly/p/4052627.html");
-				HttpClient httpclient = new DefaultHttpClient();
-				InputStream Inputstream = null;
+				StringBuffer sff = new StringBuffer();// 一定要new一个，我刚开始搞忘了，出不来。
 				try {
-					httpreponse = httpclient.execute(httpget);
-					httpentity = httpreponse.getEntity();
-					Inputstream = httpentity.getContent();
-					BufferedReader reader = new BufferedReader(
-							new InputStreamReader(Inputstream,"GB2312"));
-					String result = "";
-					String line = "";
-					while ((line = reader.readLine()) != null) {
-						result = result + line;
-						msgs = line;
-						linecount++;
-						XihaiwanActivity.this.runOnUiThread(updateThread);
+					Document doc = Jsoup.connect(
+							"http://www.cnblogs.com/agileyanly/p/4052627.html")
+							.get();
+					Elements links = doc.select("span[style]");
+					linknum = links.size();
+					int i = 0;
+					linkstr = new String[linknum];
+					week = new String[linknum];
+					url = new String[linknum];
+					// 注意这里是Elements不是Element。同理getElementById返回Element，getElementsByClass返回时Elements
+					for (Element link : links) {
+						// 这里没有什么好说的。
+						linkstr[i] = link.text();
+						String[] aa = linkstr[i].split(" ");
+						week[i] = aa[0];
+						url[i] = aa[1];
+						i++;
 					}
-					msgs = linecount+"";
+					msgs= url[0];
 					XihaiwanActivity.this.runOnUiThread(updateThread);
-
 				} catch (Exception e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} finally {
-					try {
-						Inputstream.close();
-					} catch (Exception e) {
-						e.printStackTrace();
+				}	
+				
+				
+				spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+					public void onItemSelected(AdapterView<?> parent, View view,
+							final int position, long id) {
+						String str = parent.getItemAtPosition(position).toString();
+						Toast.makeText(XihaiwanActivity.this, "你点击的是:" + position, 2000).show();
+						new Thread() {
+							public void run() {
+						try {
+							Document doc = Jsoup.connect(url[position]).get();
+							Elements links = doc.select("td[style]");
+							int linknum = links.size();
+							int i = 0;
+							linkstr = new String[linknum];
+							// 注意这里是Elements不是Element。同理getElementById返回Element，getElementsByClass返回时Elements
+							for (Element link : links) {
+								// 这里没有什么好说的。
+								linkstr[i++] = link.text();
+							}
+							msgs= url[0];
+							XihaiwanActivity.this.runOnUiThread(updateThread);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+							}}.start();
 					}
+					public void onNothingSelected(AdapterView<?> parent) {
+						// TODO Auto-generated method stub    
+						}
+				});
+
 				}
-				// 等等之类的
-			}
 		}.start();
 		
+
+	
+//				HttpGet httpget = new HttpGet(
+//						"http://www.cnblogs.com/agileyanly/p/4052627.html");
+//				HttpClient httpclient = new DefaultHttpClient();
+//				InputStream Inputstream = null;
+//				try {
+//					httpreponse = httpclient.execute(httpget);
+//					httpentity = httpreponse.getEntity();
+//					Inputstream = httpentity.getContent();
+//					BufferedReader reader = new BufferedReader(
+//							new InputStreamReader(Inputstream, "GB2312"));
+//					String result = "";
+//					String line = "";
+//					while ((line = reader.readLine()) != null) {
+//						result = result + line;
+//						msgs = line;
+//						linecount++;
+//						XihaiwanActivity.this.runOnUiThread(updateThread);
+//					}
+//					msgs = linecount + "";
+//					XihaiwanActivity.this.runOnUiThread(updateThread);
+//
+//				} catch (Exception e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} finally {
+//					try {
+//						Inputstream.close();
+//					} catch (Exception e) {
+//						e.printStackTrace();
+//					}
+//				}
+//				// 等等之类的
+
 
 	}
 }
